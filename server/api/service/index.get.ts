118cdbe745c65e348ca4f323f3764e6c0f69032c
Rohan Dhimal <drowhann@gmail.com>
2023-06-19 07:09:17 +0545

    feat: add pagination in service list

--- server/api/service/index.get.ts
+++ server/api/service/index.get.ts
@@ -1,7 +1,26 @@
+import { sql } from 'drizzle-orm'
 import { db } from '../../../drizzle/db'
 import { service } from '../../../drizzle/schema'
 
 export default defineEventHandler(async (event) => {
-  const services = db.select().from(service)
-  return services
+  const config = useRuntimeConfig()
+  const page = Number(getQuery(event)['page']) || 1
+
+  const services = await db
+    .select()
+    .from(service)
+    .limit(config.pageSize)
+    .offset((page - 1) * config.pageSize)
+
+  const servicesCount = await db.select({ count: sql<number>`count(*)` }).from(service)
+
+  return {
+    results: services,
+    pagination: {
+      page,
+      pages: Math.ceil(servicesCount[0]!.count / config.pageSize),
+      total: servicesCount[0]!.count,
+      size: config.pageSize,
+    },
+  }
 })
